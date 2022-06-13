8abefdc25d0d5ab9bc6b69c7c4a52a74d58fcf29
Hossein <hosseindozandeh799@gmail.com>
2022-06-13 12:48:51 +0430

    added script 3

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// The first and second methods
+
 // The first method
 
 // let ball = document.querySelector("#ball")
@@ -9,12 +11,36 @@
 
 
 // The second method
-let ball = document.querySelector("#ball")
-let offset = 10;
+// let ball = document.querySelector("#ball")
+// let offset = 10;
+// function animate(){
+//     ball.style.left = offset + "px"
+//     offset +=1
+//     requestAnimationFrame(animate)
+// }
+// animate()
+// The reason that the second method is better than the first method is because it is optimal
+
+
+//    The third method is the use of canvas
+
+let canvas = document.querySelector("canvas")
+
+canvas.width = window.innerWidth
+canvas.height =window.innerHeight
+
+let c = canvas.getContext("2d")
+
+let r = 40
+let x = 50
+let y = 50
+
+
 function animate(){
-    ball.style.left = offset + "px"
-    offset +=1
+    c.beginPath()
+    c.arc(x,y,r,0,2 * Math.PI)
+    c.fillStyle = "red"
+    x+= 1
     requestAnimationFrame(animate)
 }
 animate()
-// The reason that the second method is better than the first method is because it is optimal
