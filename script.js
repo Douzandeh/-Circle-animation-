a6960b00581cd8eaab6544c3a0c9810943a015bc
Hossein <hosseindozandeh799@gmail.com>
2022-06-13 12:27:03 +0430

    added script 2

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 // The first method
 
+// let ball = document.querySelector("#ball")
+// let offset = 10    //  <=== This means that our ball is 10 px away from the left
+// setInterval(() => {
+//     ball.style.left = offset + "px"
+//     offset += 1
+// } , 10)
+
+
+// The second method
 let ball = document.querySelector("#ball")
-let offset = 10    //  <=== This means that our ball is 10 px away from the left
-setInterval(() => {
+let offset = 10;
+function animate(){
     ball.style.left = offset + "px"
-    offset += 1
-} , 10)
+    offset +=1
+    requestAnimationFrame(animate)
+}
+animate()
+// The reason that the second method is better than the first method is because it is optimal
